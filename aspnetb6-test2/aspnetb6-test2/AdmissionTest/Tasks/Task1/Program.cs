8f8cc428fa98d2901901fe3f0b9f9ed52df1d17b
Arif-Shuvo <16172103307@cse.bubt.edu.bd>
2021-08-31 22:00:36 +0600

    Test file copied

---
+++ aspnetb6-test2/aspnetb6-test2/AdmissionTest/Tasks/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Show example use case of ItemBox
+        }
+    }
+}
---
+++ aspnetb6-test2/aspnetb6-test2/README.md
@@ -0,0 +1,1 @@
+# aspnetb6-test2
