26b76a0fafddc9fd7842a34a330d41f2f7c283e7
Arif-Shuvo <16172103307@cse.bubt.edu.bd>
2021-08-31 23:14:47 +0600

    Test 2

--- aspnetb6-test2/aspnetb6-test2/AdmissionTest/Tasks/Task2/Program.cs
+++ aspnetb6-test2/aspnetb6-test2/AdmissionTest/Tasks/Task2/Program.cs
@@ -13,4 +13,33 @@ namespace Task2
     }
 
     /* Write your code below this line*/
+    public static class RotateString
+    {
+        public static string Rotate(this String str, int count)
+        {
+            int len = str.Length;
+            if (len == count)
+            {
+                return str;
+            }
+            else
+            {
+                if (len < count)
+                {
+                    count = count - len;
+                }
+
+                for (int i = 0; i < count; i++)
+                {
+                    var last = str[len - 1].ToString();
+
+                    str = str.Remove(len - 1);
+
+                    str = str.Insert(0, last);
+                }
+            }
+
+            return str;
+        }
+    }
 }
